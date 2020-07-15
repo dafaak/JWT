7c0a892bcf7c2584313a937e04a224d021297f17
Israel <israel.pe@outlook.es>
2020-07-15 15:38:44 -0500

    usuario recibido
    ya no se usa el usuario quemado en el login

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
 app.get('/api', (req, res) => {
     res.json({
         message: 'Bienvenido a la API'
@@ -23,12 +30,12 @@ app.post('/api/post', verificarToken, (req, res) => {
 
 
 app.post('/api/login', (req, res) => {
-    const user = {
+    /*const user = {
         id: 1,
         username: 'Israel',
         email: 'israel@mail.com'
-    }
-
+    }*/
+    const user = req.body;
     jwt.sign({user: user}, 'secreto', (err, token) => {
         res.json({
             token
@@ -38,12 +45,14 @@ app.post('/api/login', (req, res) => {
 
 function verificarToken(req, res, next) {
     // obtener la cabecera del auth
-    const header = req.headers['autorizacion'];
+    //const header = req.headers['autorizacion'];
+    const header = req.body;
     // Verificar si el header es indefinido
     if (typeof header !== 'undefined') {
-        const bearer = header.split(' ');
+        //const bearer = header.split(' ');
         // Obtener el token
-        const bearerToken = bearer[1];
+        //const bearerToken = bearer[1];
+        const bearerToken = header.bearer;
         // Setear el token
         req.token = bearerToken;
         next();
